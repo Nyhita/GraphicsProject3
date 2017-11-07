5ada0e06a9440516410b9cdab3cfaad8bb2a1b43
Nelson Wapon Safo <nelwap97@cs.utexas.edu>
2017-11-07 13:32:55 -0600

    1st commit

---
+++ build/CMakeFiles/utgraphicsutil.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/lib/bitmap.cpp" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/CMakeFiles/utgraphicsutil.dir/lib/bitmap.cpp.o"
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/lib/debuggl.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/CMakeFiles/utgraphicsutil.dir/lib/debuggl.cc.o"
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/lib/jpegio.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/CMakeFiles/utgraphicsutil.dir/lib/jpegio.cc.o"
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/lib/mmdadapter.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/CMakeFiles/utgraphicsutil.dir/lib/mmdadapter.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/lusr/opt/glfw-3.1.2/include"
+  "/usr/include/libdrm"
+  "/usr/local/include"
+  "/opt/local/include"
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/skinning.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/src/bone_geometry.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/src/CMakeFiles/skinning.dir/bone_geometry.cc.o"
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/src/gui.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/src/CMakeFiles/skinning.dir/gui.cc.o"
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/src/main.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/src/CMakeFiles/skinning.dir/main.cc.o"
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/src/procedure_geometry.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/src/CMakeFiles/skinning.dir/procedure_geometry.cc.o"
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/src/render_pass.cc" "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/src/CMakeFiles/skinning.dir/render_pass.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/lusr/opt/glfw-3.1.2/include"
+  "/usr/include/libdrm"
+  "/usr/local/include"
+  "/opt/local/include"
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/v/filer5b/v38q001/nelwap97/Documents/CS354/Project4/skinning/build/CMakeFiles/utgraphicsutil.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
